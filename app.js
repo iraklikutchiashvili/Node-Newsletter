51cb6cf50b39d6d3d4a283e0fdd947b7eee4dded
iraklikutchiashvili <irakli9823@gmail.com>
2022-12-05 00:38:06 +0400

    Update app.js

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.post("/success", function(req, res){
     res.redirect("/")
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("The server is running on Port 3000");
 })
 
